3de3ad3d563c6b46ec335b4af7f2813f26109cd1
Apokleos <45928598+Apokleos@users.noreply.github.com>
2020-04-16 09:31:10 +0800

    Update merge_sort.go
    add new implementation for mergesort

--- sorts/merge_sort.go
+++ sorts/merge_sort.go
@@ -1,20 +1,42 @@
 //Package sorts a package for demonstrating sorting algorithms in Go
 package sorts
 
-func merge(a []int, b []int) (r []int) {
-	i, j := 0, 0
+func merge(a, b []int) (R []int) {
+	l, r := 0, 0
 
-	for i < len(a) && j < len(b) {
+        for l < len(a) && r < len(b) {
+                if a[l] < b[r] {
+                        R = append(R, a[l])
+                        l++
+                } else {
+                        R = append(R, b[r])
+                        r++
+                }
+        }
 
-		if a[i] <= b[j] {
-			r[i+j] = a[i]
-			i++
-		} else {
-			r[i+j] = b[j]
-			j++
-		}
+        R = append(R, b[r:]...)
+        R = append(R, a[l:]...)
 
-	}
+        return R
+
+}
+
+// func merge(a []int, b []int) []int {
+	
+// 	r := make([]int, len(a)+len(b))
+// 	i, j := 0, 0
+
+// 	for i < len(a) && j < len(b) {
+
+// 		if a[i] <= b[j] {
+// 			r[i+j] = a[i]
+// 			i++
+// 		} else {
+// 			r[i+j] = b[j]
+// 			j++
+// 		}
+
+// 	}
 
 // 	for i < len(a) {
 // 		r[i+j] = a[i]
@@ -24,10 +46,9 @@ func merge(a []int, b []int) (r []int) {
 // 		r[i+j] = b[j]
 // 		j++
 // 	}
-	r = append(r, a[i+j:])
-	r = append(r, b[i+j:])
-	return r
-}
+
+// 	return r
+// }
 
 //Mergesort Perform mergesort on a slice of ints
 func Mergesort(items []int) []int {
